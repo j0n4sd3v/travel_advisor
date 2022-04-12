d138864c8aff88f927800b46ec7d943dbdb5967f
jonathan <contacto@jonasdev.es>
2022-04-12 22:10:18 +0200

    First Commit2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { ReactDOM } from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+const root=createRoot(document.getElementById('root'));
+
+root.render(<App/>);
