7e3f8d982e031a5ec283b079033c89e55851a6a1
jonathan <contacto@jonasdev.es>
2022-04-13 18:01:44 +0200

    initial folder structure & layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import {CssBaseline,Grid}from '@mui/material';
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+import Map from './Components/Map/Map';
+import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
 
 const App =()=>{
     return(
-        <div>
-            <h1>Hello world!</h1>
-        </div>
+        <>
+            <CssBaseline/>
+            <Header/>
+            <Grid container spacing={3} style={{width:'100%'}}>
+                <Grid item xs={12} md={4} >
+                    <List/>
+                </Grid>
+                <Grid item xs={12} md={8} >
+                    <Map/>
+                </Grid>
+            </Grid>
+        </>
     )
 }
 export default App;
