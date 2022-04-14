3ff60af0cc058a1f857b3ef4ec76633ada4dc9e2
jonathan <contacto@jonasdev.es>
2022-04-14 19:23:18 +0200

    mui settings

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Header from './Components/Header/Header';
 import List from './Components/List/List';
 import Map from './Components/Map/Map';
 import PlaceDetails from './Components/PlaceDetails/PlaceDetails';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme=createTheme({});
 
 const App =()=>{
     return(
         <>
-            <CssBaseline/>
-            <Header/>
-            <Grid container spacing={3} style={{width:'100%'}}>
-                <Grid item xs={12} md={4} >
-                    <List/>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <Header/>
+                <Grid container spacing={3} style={{width:'100%'}}>
+                    <Grid item xs={12} md={4} >
+                        <List/>
+                    </Grid>
+                    <Grid item xs={12} md={8} >
+                        <Map/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={8} >
-                    <Map/>
-                </Grid>
-            </Grid>
+            </ThemeProvider>
         </>
     )
 }
