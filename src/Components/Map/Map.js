195665a22c1d9c692dbd154fe6b0999bcec221a9
jonathan <contacto@jonasdev.es>
2022-04-14 20:53:50 +0200

    Map commit

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -4,7 +4,10 @@ import { Typography, Paper, useMediaQuery,} from '@mui/material';
 import LocationOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import Rating from '@mui/material/Rating';
 
+import useStyle from './style';
+
 const Map=()=>{
+    const classes=useStyle();
     return(
         <div>
             Map     
