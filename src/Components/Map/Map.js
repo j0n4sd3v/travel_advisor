d9bcc029fbee6a48fe3c4e51d19e1ea46e7aac52
jonathan <contacto@jonasdev.es>
2022-04-26 21:08:09 +0200

    maps1

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,15 +1,30 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
 import { Typography, Paper, useMediaQuery,} from '@mui/material';
-import LocationOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import LocationOnOutlined from '@mui/icons-material/LocationOnOutlined';
 import Rating from '@mui/material/Rating';
 
 import useStyle from './style';
 
 const Map=()=>{
     const classes=useStyle();
+    const isMobile=useMediaQuery('(min-width:600px)');
+
+    const coordinates= {lat:0,lon:0};
     return(
-        <div>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{key:'AIzaSyAcEYN2PDk7XMgfvLaIb-pWBm8SFCjKCUk'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
             Map     
         </div>
     )
